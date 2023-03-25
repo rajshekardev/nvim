c1a0bf172940eb8395905dfead1865195b205602
rajshekardev <115916866+rajshekardev@users.noreply.github.com>
2023-03-25 09:34:41 +0530

    Neovide removed fullscreen

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 -- require("core.set")
 require("core.lazy")
 require("core.set")
+
+if vim.g.neovide then
+	require("core.neovide")
+end
---
+++ lua/core/neovide.lua
@@ -0,0 +1,13 @@
+print("neovide?")
+
+vim.opt.linespace = 0
+
+vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 0
+vim.g.neovide_padding_left = 0
+
+vim.g.neovide_refresh_rate_idle = 5
+
+vim.g.neovide_cursor_vfx_mode = "railgun"
+
